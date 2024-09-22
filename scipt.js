6c648e6bcaccb12061a32a6b922a6b53ea8bb8ef
Zeox4 <59748901+Zeox4@users.noreply.github.com>
2024-09-21 18:26:41 -0600

    index y css update

---
+++ scipt.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const modal = document.getElementById("image-modal");
+    const modalImg = document.getElementById("modal-image");
+    const closeBtn = document.getElementsByClassName("close-btn")[0];
+
+    // Agregar evento de clic a cada imagen
+    document.querySelectorAll('.clickable-image').forEach(image => {
+        image.addEventListener('click', function() {
+            modal.style.display = "flex"; // Mostrar modal
+            modalImg.src = this.src; // Establecer la imagen en el modal
+        });
+    });
+
+    // Cerrar el modal cuando se hace clic en la "X"
+    closeBtn.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // Cerrar modal al hacer clic fuera de la imagen
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+});
